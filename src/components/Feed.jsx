081b1ee299b5d0d57bd2b143729ede6629ca0ff6
x-farzan <farzanhassan245@gmail.com>
2023-02-26 22:48:03 +0500

    Siderbar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Sidebar } from ".";
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2020
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
 export default Feed;
